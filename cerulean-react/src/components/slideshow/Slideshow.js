70c6d9808eb16f7d5a6269592d20f656a7283a69
Sophia Sherman <sherman205@gmail.com>
2018-11-04 18:04:41 -0500

    Add react-image-gallery npm module for slideshow component

--- cerulean-react/src/components/slideshow/Slideshow.js
+++ cerulean-react/src/components/slideshow/Slideshow.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
+import ImageGallery from 'react-image-gallery';
+import "../../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 import './Slideshow.css';
 
 class Slideshow extends Component {
+
+	handleImageLoad(event) {
+    console.log('Image loaded ', event.target)
+  }
+
 	render() {
-	    return (
-			<div className="slideshow">
-		    </div>
-		);
-	}
+		const images = [
+      {
+        original: 'http://via.placeholder.com/500x250',
+        thumbnail: 'http://via.placeholder.com/500x250',
+        originalClass: 'featured-slide',
+        thumbnailClass: 'featured-thumb',
+        originalAlt: 'original-alt',
+        thumbnailAlt: 'thumbnail-alt',
+        thumbnailLabel: 'Optional',
+        description: 'Optional description...',
+      },
+      {
+        original: 'http://via.placeholder.com/500x250',
+        thumbnail: 'http://via.placeholder.com/500x250'
+      },
+      {
+        original: 'http://via.placeholder.com/500x250',
+        thumbnail: 'http://via.placeholder.com/500x250'
+      }
+    ]
+
+    return (
+    	<div className="gallery">
+    		<ImageGallery
+	        ref={i => this._imageGallery = i}
+	        items={images}
+	        slideInterval={100}
+	        onImageLoad={this.handleImageLoad}/>
+    	</div>
+    );
+  }
 }
 
 export default Slideshow;
