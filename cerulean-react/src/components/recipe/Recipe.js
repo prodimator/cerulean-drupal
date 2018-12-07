268ae74d3d6b52480841e5b627ec07187181b00d
Ryan <rwolfe22@gmail.com>
2018-12-06 20:01:21 -0500

    flexbox on recipe page

--- cerulean-react/src/components/recipe/Recipe.js
+++ cerulean-react/src/components/recipe/Recipe.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
 import { _AppConstants } from '../../index.constants';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 import './Recipe.css';
 
 
 class Recipe extends Component {
   constructor(props) {
-      super(props);
-      this.state = {
-        recipes: []
-      };
-    }
+    super(props);
+    this.state = {
+      recipes: []
+    };
+  }
 
   componentDidMount() {
-  fetch(_AppConstants.api + '/api/recipes?_format=json')
-    .then((results) => {
-      return results.json();
-    }).then((data) => {
-      let recipes = data.map(recipe => {
-        let parsedTitle = recipe.title.split(' ');
-        return ({
-          id: recipe.id,
-          title: recipe.title,
-          title_bold: parsedTitle[0]+parsedTitle[1],
-          title_slim: parsedTitle[2],
-          date: recipe.date,
-          description: recipe.description,
-          body_content: recipe.body_content,
-          ingredients: recipe.ingredients,
-          instructions: recipe.instructions,
-          image_header_url: recipe.image_header,
-        });
+    fetch(_AppConstants.api + '/api/recipes?_format=json' + '&id='+ this.props.match.params.id)
+      .then((results) => {
+        return results.json();
+      }).then((data) => {
+        let recipes = data.map(recipe => {
+          let parsedTitle = recipe.title.split(' ');
+          return ({
+            id: recipe.id,
+            title: recipe.title,
+            title_bold: parsedTitle[0] + parsedTitle[1],
+            title_slim: parsedTitle[2],
+            date: recipe.date,
+            description: recipe.description,
+            body_content: recipe.body_content,
+            ingredients: recipe.ingredients,
+            instructions: recipe.instructions,
+            image_header_url: recipe.image_header,
+          });
+        })
+        this.setState({ recipes });
       })
-      this.setState({recipes});
-    })
   }
 
   render() {
@@ -45,21 +47,45 @@ class Recipe extends Component {
 
     return (
       <div>
-        <header className="recipe-header" style={{backgroundImage: `url(${_AppConstants.api}${recipe.image_header_url})`}} />
-        <div className="recipe-post">
-          <div className="recipe-title title-bold">
-            {recipe.title_bold}
-          </div>
-          <div className="recipe-title title-slim">
-            {recipe.title_slim}
-          </div>
-          <div className="recipe-body" dangerouslySetInnerHTML={{ __html: recipe.body_content }} />
-          <h3>Ingredients</h3>
-          <div className="recipe-ingredients" dangerouslySetInnerHTML={{ __html: recipe.ingredients }} />
-          <h3>Instructions</h3>
-          <div className="recipe-instructions" dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
-        </div>
-        </div>
+        <Row>
+          <Col md={12}>
+            <header className="recipe-header" style={{ backgroundImage: `url(${_AppConstants.api}${recipe.image_header_url})` }} />
+          </Col>
+        </Row>
+        <Row center="md">
+          <Col lg={9}>
+            <div className="recipe-post">
+              <Row center="md">
+                <Col md>
+                  <div className="recipe-title title-bold">
+                    {recipe.title_bold}
+                  </div>
+                  <div className="recipe-title title-slim">
+                    {recipe.title_slim}
+                  </div>
+                </Col>
+              </Row>
+              <Row start="md">
+                <Col md>
+                  <div className="recipe-body" dangerouslySetInnerHTML={{ __html: recipe.body_content }} />
+                </Col>
+              </Row>
+              <Row start="md">
+                <Col md={6} className="recipe-ingredients">
+                  <h3>Ingredients</h3>
+                  <div dangerouslySetInnerHTML={{ __html: recipe.ingredients }} />
+                </Col>
+              </Row>
+              <Row start="md">
+                <Col md={6} className="recipe-instructions">
+                  <h3>Instructions</h3>
+                  <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
