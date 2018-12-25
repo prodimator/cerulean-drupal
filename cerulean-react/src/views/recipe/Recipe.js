ff2b37f8e1e015cfaa1b55c123846dedb0c6df66
Ryan <rwolfe22@gmail.com>
2018-12-25 17:07:36 -0500

    added mobile specific render to recipe page

--- cerulean-react/src/views/recipe/Recipe.js
+++ cerulean-react/src/views/recipe/Recipe.js
@@ -11,9 +11,20 @@ class Recipe extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipes: []
+      recipes: [],
+      width: window.innerWidth
     };
   }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
 
   componentDidMount() {
     fetch(_AppConstants.api + '/api/recipes?_format=json' + '&id='+ this.props.match.params.id)
@@ -40,6 +51,9 @@ class Recipe extends Component {
   }
 
   render() {
+    const { width } = this.state;
+    const isMobile = width <= 500;
+
     let id = this.props.match.params.id;
     let recipe = this.state.recipes.reduce(function (current, recipe) {
       return id === current.id ? current : recipe;
@@ -47,54 +61,90 @@ class Recipe extends Component {
 
     if (!recipe) return <div>Image not found</div>;
 
-    return (
-      <div>
-        <Hamburger />
-        <HamburgerNav />
-        <Row>
-          <Col md={12}>
-            <header className="recipe-header" style={{ backgroundImage: `url(${_AppConstants.api}${recipe.image_header_url})` }} />
-          </Col>
-        </Row>
-        <Row center="md">
-          <Col lg={9}>
-            <div className="recipe-post">
-              <Row start="md">
-                <Col md>
-                  <div className="title title-bold">
-                    {recipe.title_bold}
-                  </div>
-                  <div className="title title-slim">
-                    {recipe.title_slim}
-                  </div>
-                </Col>
-              </Row>
-              <Row start="md">
-                <Col md>
-                  <div className="recipe-body" dangerouslySetInnerHTML={{ __html: recipe.body_content }} />
-                </Col>
-              </Row>
-              <Row start="md" className="recipe-info">
-                <Col md={6} className="recipe-ingredients">
-                  <h3>Ingredients</h3>
-                  <div dangerouslySetInnerHTML={{ __html: recipe.ingredients }} />
-                </Col>
-                <Col md={6} className="recipe-instructions">
-                  <h3>Instructions</h3>
-                  <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
-                </Col>
-              </Row>
-              {/* <Row start="md">
-                <Col md={6} className="recipe-instructions">
-                  <h3>Instructions</h3>
-                  <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
-                </Col>
-              </Row> */}
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
+    if (!isMobile){
+      return (
+        <div>
+          <Hamburger />
+          <HamburgerNav />
+          <Row>
+            <Col md={12}>
+              <header className="recipe-header" style={{ backgroundImage: `url(${_AppConstants.api}${recipe.image_header_url})` }} />
+            </Col>
+          </Row>
+          <Row center="md">
+            <Col lg={9}>
+              <div className="recipe-post">
+                <Row start="md">
+                  <Col md>
+                    <div className="title title-bold">
+                      {recipe.title_bold}
+                    </div>
+                    <div className="title title-slim">
+                      {recipe.title_slim}
+                    </div>
+                  </Col>
+                </Row>
+                <Row start="md">
+                  <Col md>
+                    <div className="recipe-body" dangerouslySetInnerHTML={{ __html: recipe.body_content }} />
+                  </Col>
+                </Row>
+                <Row start="md" className="recipe-info">
+                  <Col md={6} className="recipe-ingredients">
+                    <h3>Ingredients</h3>
+                    <div dangerouslySetInnerHTML={{ __html: recipe.ingredients }} />
+                  </Col>
+                  <Col md={6} className="recipe-instructions">
+                    <h3>Instructions</h3>
+                    <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      );
+    }
+
+    if (isMobile){
+      return(
+        <div className="mobile">
+          <Hamburger />
+          <HamburgerNav />
+          <Row>
+            <Col>
+              <img className="mobile-header-img" src={_AppConstants.api + recipe.image_header_url}></img>
+            </Col>
+          </Row>
+          <Row center="xs" middle="xs">
+            <Col xs={10}>
+              <div className="title-mobile title-bold">
+                {recipe.title_bold}
+              </div>
+              <div className="title-mobile title-slim">
+                {recipe.title_slim}
+              </div>
+            </Col>
+          </Row>
+          <Row start="sm">
+            <Col xs>
+              <div className="recipe-body-mobile" dangerouslySetInnerHTML={{ __html: recipe.body_content }} />
+            </Col>
+          </Row>
+          <Row start="sm" className="recipe-info-mobile">
+            <Col md={10}>
+              <h3>Ingredients</h3>
+              <div dangerouslySetInnerHTML={{ __html: recipe.ingredients }} />
+            </Col>
+            <Col md={10}>
+              <h3>Instructions</h3>
+              <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+            </Col>
+          </Row>
+        </div>
+        
+      )
+    }
   }
 }
 
