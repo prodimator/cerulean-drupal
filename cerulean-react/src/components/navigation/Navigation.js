e55e11078b99eca55de5e7df231d89ce4f5b05a3
Sophia Sherman <sherman205@gmail.com>
2019-01-03 22:14:12 -0500

    Add basic searchbar autocompletion

--- cerulean-react/src/components/navigation/Navigation.js
+++ cerulean-react/src/components/navigation/Navigation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Route, Link } from "react-router-dom";
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
+import { _AppConstants } from '../../index.constants';
 import './Navigation.css';
 
 import MailIcon from '../../svg/mail.svg';
@@ -10,8 +11,145 @@ import InstaIcon from '../../svg/instagram.svg';
 import LogoHeader from '../../images/logo_header.png';
 
 
-class Nav extends Component {
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // The active selection's index
+      activeSuggestion: 0,
+      // The suggestions that match the user's input
+      filteredSuggestions: [],
+      // Whether or not the suggestion list is shown
+      showSuggestions: false,
+      // What the user has entered
+      userInput: "",
+      suggestions: [],
+      results: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(_AppConstants.api + '/api/recipes?_format=json')
+      .then((results) => {
+        return results.json();
+      }).then((data) => {
+        let suggestions = data.map(recipe => {
+          return (recipe.title);
+        })
+        this.setState({suggestions});
+      })
+  }
+
+  onKeyDown = e => {
+    const { activeSuggestion, filteredSuggestions } = this.state;
+
+    // User pressed the enter key, update the input and close the
+    // suggestions
+    if (e.keyCode === 13) {
+      this.setState({
+        activeSuggestion: 0,
+        showSuggestions: false,
+        userInput: filteredSuggestions[activeSuggestion]
+      });
+    }
+    // User pressed the up arrow, decrement the index
+    else if (e.keyCode === 38) {
+      if (activeSuggestion === 0) {
+        return;
+      }
+
+      this.setState({ activeSuggestion: activeSuggestion - 1 });
+    }
+    // User pressed the down arrow, increment the index
+    else if (e.keyCode === 40) {
+      if (activeSuggestion - 1 === filteredSuggestions.length) {
+        return;
+      }
+
+      this.setState({ activeSuggestion: activeSuggestion + 1 });
+    }
+  };
+
+  onChange = e => {
+    console.log("this is gonna be a sick test man");
+    console.log(this.state.suggestions);
+    const userInput = e.currentTarget.value;
+
+    // Filter our suggestions that don't contain the user's input
+    const filteredSuggestions = this.state.suggestions.filter(
+      suggestion =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    // Update the user input and filtered suggestions, reset the active
+    // suggestion and make sure the suggestions are shown
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions,
+      showSuggestions: true,
+      userInput: e.currentTarget.value
+    });
+  };
+
+  onClick = e => {
+    // Update the user input and reset the rest of the state
+    this.setState({
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: e.currentTarget.innerText
+    });
+  };
+
   render() {
+    const {
+      onChange,
+      onClick,
+      onKeyDown,
+      state: {
+        activeSuggestion,
+        filteredSuggestions,
+        showSuggestions,
+        userInput
+      }
+    } = this;
+
+    let suggestionsListComponent;
+
+    if (showSuggestions && userInput) {
+      if (filteredSuggestions.length) {
+        suggestionsListComponent = (
+          <ul class="suggestions">
+            {filteredSuggestions.map((suggestion, index) => {
+              let className;
+
+              // Flag the active suggestion with a class
+              if (index === activeSuggestion) {
+                className = "suggestion-active";
+              }
+
+              return (
+                <li
+                  className={className}
+                  key={suggestion}
+                  onClick={onClick}
+                >
+                  {suggestion}
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        suggestionsListComponent = (
+          <div class="no-suggestions">
+            <em>Yikes, no results found!</em>
+          </div>
+        );
+      }
+    }
     return (
       <div>
         <div className="row custom-nav">
@@ -44,7 +182,14 @@ class Nav extends Component {
               >
                 <img src={LogoHeader} className="logo-header" alt="Logo header"/>
               </Link>
-              <input placeholder="SEARCH" type="text" />
+              <input 
+                placeholder="SEARCH" 
+                type="text"
+                value={userInput}
+                onKeyDown={onKeyDown}
+                onChange={onChange}
+              />
+              {suggestionsListComponent}
             </div>
             <div className="col-container col-4 nav-social">
               <a target="_blank" rel="noopener noreferrer" href="http://www.google.com">
@@ -63,4 +208,4 @@ class Nav extends Component {
   }
 }
 
-export default Nav;
+export default Search;
