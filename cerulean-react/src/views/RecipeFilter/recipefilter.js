a5c274c2adcead7b4a0ec97695e4a3ec160ee054
Sophia <sherman205@gmail.com>
2019-05-22 14:42:58 -0700

    Add error handling for hitting taxonomy endpoint when no search results found

--- cerulean-react/src/views/RecipeFilter/recipefilter.js
+++ cerulean-react/src/views/RecipeFilter/recipefilter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import RecipePreview from '../../components/recipePreview/recipePreview';
 import * as CONSTANTS from '../../Constants';
 import axios from 'axios';
 
@@ -10,7 +11,8 @@ export default class RecipeFilter extends Component {
 	    super(props);
 	    this.state = {
 	    	filter: this.props.match.params.filter,
-	    	recipes: []
+			recipes: [],
+			error: null
 	    };
 	}
 
@@ -27,19 +29,29 @@ export default class RecipeFilter extends Component {
 		    		recipes: recipes
 		    	});
 			})
+			.catch(error => {
+				this.setState({error});
+				console.log("There was an error!");
+			})
 	}
 
 	render() {
-		return (
-			<div>
-				<h3>Your search results:</h3>
-				{this.state.recipes.map((recipe, index) => (
-					<div key={index}>
-						{recipe.title}
-					</div>
-				))}
-			</div>
-
-		);
+		if (this.state.error) {
+			return (
+				<h3>No results found</h3>
+			);
+		}
+		else {
+			return (
+				<div>
+					<h3>Your search results:</h3>
+					{this.state.recipes.map((recipe, index) => (
+						<div key={index}>
+							<RecipePreview id={recipe.id} />
+						</div>
+					))}
+				</div>
+			);
+		}
 	}
 }
