de8ffd1e9411072c52407304fab45bc8f1222f90
Sophia <sherman205@gmail.com>
2019-05-23 23:16:55 -0700

    Using webform_rest module, add post endpoint to submit contact form from contact page

--- cerulean-react/src/views/Contact/contact.js
+++ cerulean-react/src/views/Contact/contact.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import NavSmall from '../../components/navSmall/navSmall';
 import NavMobile from '../../components/navMobile/navMobile';
 import FooterSmall from '../../components/footerSmall/footerSmall';
+import * as CONSTANTS from '../../Constants';
+import axios from 'axios';
 import './contact.scss';
 
 
@@ -23,10 +25,53 @@ export default class Contact extends Component {
         this.setState({ width: window.innerWidth });
     };
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const name = e.target.name.value;
+        const subject = e.target.subject.value;
+        const message = e.target.message.value;
+
+        axios.get(CONSTANTS.BASE_URL + '/session/token').then(result => {
+            console.log(result);
+            if (result.status === 200 ) {
+                const csrfToken = result.data;
+                fetch(CONSTANTS.BASE_URL + '/webform_rest/submit?_format=json', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrfToken
+                  },
+                  body: JSON.stringify({
+                    'webform_id':'contact',
+                    'entity_type' : null,
+                    'entity_id' : null,
+                    'in_draft' : false,
+                    'uri' : ['/webform/webform_id/api'],
+                    'name' : name,
+                    'email' : email,
+                    'subject' : subject,
+                    'message': message,
+                  }),
+                })
+                .then(response => response.json())
+                .then(
+                  (data) => {
+                    //on success you do whatever you will want
+                    console.log('success', data);
+                  },
+                  (error) => {
+                    console.log('error', error);
+                  }
+                );
+            }
+        });
+    }
+
     render() {
         const { width } = this.state;
         const isMobile = width <= 650;
-        const textDisplay = "Like what you see? Please us a message and tell us what you think! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        const textDisplay = "Like what you see? Please send us a message and tell us what you think! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 
         if (!isMobile) {
             return (
@@ -42,7 +87,7 @@ export default class Contact extends Component {
                             </div>
                         </div>
                         <div className="contact-right">
-                            <form>
+                            <form onSubmit={this.handleSubmit}>
                                 <p>Your Email</p>
                                 <input type="text" name="email" />
                                 <p>Your Name</p>
@@ -51,7 +96,7 @@ export default class Contact extends Component {
                                 <input type="text" name="subject" />
                                 <p>Message</p>
                                 <textarea name="message" />
-                                <button>Submit</button>
+                                <button type="submit">Submit</button>
                             </form>
                         </div>
                     </div>
