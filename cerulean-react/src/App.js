b31243fbc9cc93b73a251b6868f66a1f7337afe3
Sophia <sherman205@gmail.com>
2019-07-07 19:24:10 -0700

    Add Google Analytics for page views

--- cerulean-react/src/App.js
+++ cerulean-react/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 import Home from './views/Home/home';
 import Recipe from './views/Recipe/recipe';
 import About from './views/About/about'
@@ -8,11 +10,19 @@ import RecipeFilter from './views/RecipeFilter/recipefilter'
 import RecipeCategories from './views/recipeCategories/recipeCategories';
 import './App.scss';
 
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.initialize('UA-143287896-1');
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <div className="App">
-
         <Route exact path="/" component={Home} />
         <Route exact path="/recipes" component={RecipeCategories} />
         <Route exact path="/about" component={About} />
